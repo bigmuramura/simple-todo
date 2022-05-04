4ba53a708aaca339801d4e7c25dc0aa0e2684189
Ohmura Yasutaka <ohmura.yasutaka@classmethod.jp>
2022-05-04 18:20:37 +0900

    Todo機能実装

---
+++ app/models/todos.go
@@ -0,0 +1,93 @@
+package models
+
+import (
+	"log"
+	"time"
+)
+
+type Todo struct {
+	ID        int
+	Content   string
+	UserID    int
+	CreatedAt time.Time
+}
+
+func (u *User) CreateTodo(content string) (err error) {
+	cmd := `insert into todos (content, user_id, created_at) values (?,?,?)` // ?のプレースホルダはMySQL, PostgreSQLは %N。SQliteはどちでも可
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos where id = ?`
+	todo = Todo{}
+
+	err = Db.QueryRow(cmd, id).Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt) //理解できていない
+
+	return todo, err
+}
+
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt) //理解できていない
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close() //rowsはなぜ閉じるのか？
+	/*
+		Closeしないとコネクション貼ったままになる。そのため、defer rows.Close() の方が良い書き方のようだ。
+		参考: https://golang.shop/post/go-databasesql-04-retrieving-ja/
+	*/
+
+	return todos, err
+}
+
+func (u *User) GetTodoByUser() (todos []Todo, err error) {
+	cmd := `select id, content,user_id,created_at from todos where user_id = ?`
+
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	return todos, err
+
+}
+
+func (t *Todo) UpdateTodo() error {
+	cmd := `update todos set content = ?, user_id = ? where id =?`
+	_, err = Db.Exec(cmd, t.Content, t.UserID, t.ID)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+}
+
+func (t *Todo) DeleteTodo() error {
+	cmd := `delete from todos where id = ?`
+	_, err := Db.Exec(cmd, t.ID)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+}
