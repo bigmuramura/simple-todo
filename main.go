64141bd511eb76189af96a6f338a2c0720b80d97
Ohmura Yasutaka <ohmura.yasutaka@classmethod.jp>
2022-05-04 09:18:51 +0900

    user取得

--- main.go
+++ main.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/bigmuramura/simple-todo/app/models"
-	"github.com/bigmuramura/simple-todo/todo_app/config"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	/*
+		fmt.Println(config.Config.Port)
+		fmt.Println(config.Config.SQLDriver)
+		fmt.Println(config.Config.DbName)
+		fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+		log.Println("test")
+	*/
 
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@examlpe.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@examlpe.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
 
-	u.CreateUser()
+		u.CreateUser()
+	*/
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
