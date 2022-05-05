9ead0cc27e1ca5cccff8fc7ce91d7e71f0c48555
Ohmura Yasutaka <ohmura.yasutaka@classmethod.jp>
2022-05-05 09:30:17 +0900

    Boostrap, JQuery追加

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -7,6 +7,9 @@ import (
 )
 
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
 
 	return http.ListenAndServe(":"+config.Config.Port, nil) // 第2引数の nil はマルチプレクサの指定で通常はデフォルトのマルチプレクサを使うため nil を指定する。登録されていないURLにアクセスされたら403を返す。
